629fc32f62e7c12a9055da2eb14f862928994e6b
Fernando Vinícius Soldera Soldera <fernandosoldera@hotmail.com>
2020-07-11 12:34:32 -0300

    Include standard lint

--- src/components/OdicHeader.js
+++ src/components/OdicHeader.js
@@ -1,55 +1,50 @@
-import React, { useState, useRef } from 'react'
-import { Carousel, Row, Col, Layout, Menu, Tabs, Button } from 'antd';
+import React, { useState } from 'react'
+import { Menu } from 'antd'
 import './header-style.css'
 
 import MenuIcon from '../assets/menuIcon.svg'
+import { Link } from 'react-router-dom'
 
-
-import {
-    Link
-} from "react-router-dom";
-
-const { Header, Footer, Sider, Content } = Layout;
 const centerStyle = {
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    backgroundColor: 'transparent',
-    border: 0,
-    color: '#FFFFFF',
-    height: 45
-  }
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  backgroundColor: 'transparent',
+  border: 0,
+  color: '#FFFFFF',
+  height: 45
+}
 
-export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false)
-    
-    return (
-        <div className='header'>
-            <div className={isOpen ? 'open' : ''} id='menuHorizontal'>
-                <Menu mode="horizontal" className='basic' style={centerStyle}>
-                    <Menu.Item key="1">
-                        <Link to="/"><p className='basic'>Home</p></Link>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to="/about"><p className='basic'>Sobre</p></Link>
-                    </Menu.Item>
-                    <Menu.Item key="5">
-                        <Link to="/portfolio"><p className='basic'>Portifólio</p></Link>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                    <Link to="/services"><p className='basic'>Serviços</p></Link>
-                    </Menu.Item>
-                    <Link to="/contact">
-                        <button className='contact'>
-                            <text>Contato</text>
-                        </button>
-                    </Link>
-                </Menu>
-            </div>
-            <div onClick={() => setIsOpen(!isOpen)} className='menuButton basic'>
-                <img src={MenuIcon}></img>
-                <text className='menuText'>Menu</text>
-            </div>
-        </div>
-    )
+export default function Navbar () {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className='header'>
+      <div className={isOpen ? 'open' : ''} id='menuHorizontal'>
+        <Menu mode='horizontal' className='basic' style={centerStyle}>
+          <Menu.Item key='1'>
+            <Link to='/'><p className='basic'>Home</p></Link>
+          </Menu.Item>
+          <Menu.Item key='2'>
+            <Link to='/about'><p className='basic'>Sobre</p></Link>
+          </Menu.Item>
+          <Menu.Item key='5'>
+            <Link to='/portfolio'><p className='basic'>Portifólio</p></Link>
+          </Menu.Item>
+          <Menu.Item key='3'>
+            <Link to='/services'><p className='basic'>Serviços</p></Link>
+          </Menu.Item>
+          <Link to='/contact'>
+            <button className='contact'>
+              <text>Contato</text>
+            </button>
+          </Link>
+        </Menu>
+      </div>
+      <div onClick={() => setIsOpen(!isOpen)} className='menuButton basic'>
+        <img src={MenuIcon} />
+        <text className='menuText'>Menu</text>
+      </div>
+    </div>
+  )
 }
