00cb4192d509a4d9b1523f72b6603fa8c9f4f6b2
Fernando Vinícius Soldera Soldera <fernandosoldera@hotmail.com>
2020-07-14 00:30:12 -0300

    Implement services route

--- src/pages/Portifolio/Design/index.js
+++ src/pages/Portifolio/Design/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import OdicHeader from '../../../components/Header/OdicHeader'
 import PortifolioHeader from '../../../components/PortifolioHeader/PortifolioHeader'
--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,38 +1,57 @@
 import React, { useState } from 'react'
-import OdicHeader from '../../components/Header/OdicHeader';
-import { Carousel, Row, Col, Layout, Menu, Tabs } from 'antd'
-const { TabPane } = Tabs
-const { Header, Footer, Sider, Content } = Layout
+import OdicHeader from '../../components/Header/OdicHeader'
+import { Row, Col } from 'antd'
+import './servicesStyle.css'
 
+import RightArrow from '../../assets/rightArrow.svg'
+import { Link } from 'react-router-dom'
 
+export default function About () {
+  const [textSelecioned, setTextSelecioned] = useState(1)
 
-export default function Services () {
   return (
-    <Layout className='layout'>
-      <Content>
-        <Row className='back'>
-          <Col span={12}>
-            <div className='left-col'>
-              <h1 className='title2'>Serviços</h1>
-              <h4 className='sub-title2'>Aplicativos mobile</h4>
-              <h4 className='sub-title2'>Desenvolvimento web</h4>
-              <h4 className='sub-title2'>Criação de Design</h4>
+    <div className='services'>
+      <OdicHeader />
+      <Row>
+        <Col span={11} className='leftCol'>
+          <text>Serviços</text>
+
+          <button onClick={() => setTextSelecioned(1)}>
+              Aplicativos Mobile
+            <div className={textSelecioned === 1 ? 'buttonBorder' : ''} />
+          </button>
+
+          <button onClick={() => setTextSelecioned(2)}>
+              Desenvolvimento Web
+            <div className={textSelecioned === 2 ? 'buttonBorder' : ''} />
+          </button>
+
+          <button onClick={() => setTextSelecioned(3)}>
+              Criação de Design
+            <div className={textSelecioned === 3 ? 'buttonBorder' : ''} />
+          </button>
+
+        </Col>
+        <Col span={13} className='rightCol'>
+          <div className={textSelecioned !== 1 ? 'hide' : ''}>
+            <text className='aboutFirstText'>Nós criamos aplicativos mobile com foco em funcionalidade, estética e interatividade. Utilizamos as tecnologias mais modernas que proporcionam a melhor experiência ao usuário/cliente final!</text>
+          </div>
+          <div className={textSelecioned !== 2 ? 'hide' : ''}>
+            <text>Nós desenvolvemos sites modernos, eficientes e bem interativos para você e seu usuário/cliente!</text>
+          </div>
+          <div className={textSelecioned !== 3 ? 'hide' : ''}>
+            <text>Nós criamos ou atualizamos a identidade visual da sua ideia para qualquer formato digital ou impresso de sua escolha, usando consistência, criatividade e toque de excepcionalidade.</text>
+          </div>
+          <Link to='/contact'>
+            <div className='divButton'>
+              <button>
+              Entrar em contato
+                <img src={RightArrow} />
+              </button>
             </div>
-          </Col>
-          <Col span={12}>
-            <OdicHeader />
-            <div>
-              <Carousel autoplay>
-                <div>
-                  <p>
-                                        Nós criamos aplicativos mobile com foco em funcionalidade, estética e  interatividade. Utilizamos as tecnologias mais modernas que proporcionam a melhor experiência ao  usuário/cliente final!
-                  </p>
-                </div>
-              </Carousel>
-            </div>
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
+          </Link>
+        </Col>
+      </Row>
+    </div>
   )
 }
