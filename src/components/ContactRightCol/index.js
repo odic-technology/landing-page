db7f87a8665d298e196b8d8d3ee321092fdaf700
Fernando Vinícius Soldera Soldera <fernandosoldera@hotmail.com>
2020-07-16 13:39:08 -0300

    Contact state

---
+++ src/components/ContactRightCol/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import CheckIcon from '../../assets/checkIcon.svg'
+import RightArrow from '../../assets/rightArrow.svg'
+
+import './contactRightColStyle.css'
+
+export default function ContactRightColumn ({ setTypeSelected, typeSelected, isFirstSelected, isSecondSelected, isThirdSelected, selectItem, quantitySelected, setIsFormAvailable }) {
+  return (
+    <>
+      <div className='divText'>
+        <text>Gostaria de criar algo conosco?</text>
+        <text><text>É muito simples</text>, basta selecionar as</text>
+        <text>opções abaixo:</text>
+      </div>
+      <div className='cardContact'>
+        <div className='cardContactHeader'>
+          <div className='cardContactType'>
+            <text onClick={() => setTypeSelected(1)} className={typeSelected === 1 ? 'headerSelected' : ''}>Web</text>
+            <div className={typeSelected === 1 ? 'headerSelectedTriangle' : ''} />
+            <img src={CheckIcon} className={isFirstSelected ? 'headerSelectedCheck' : ''} />
+          </div>
+          <div className='cardContactType'>
+            <text onClick={() => setTypeSelected(2)} className={typeSelected === 2 ? 'headerSelected' : ''}>Mobile</text>
+            <div className={typeSelected === 2 ? 'headerSelectedTriangle' : ''} />
+            <img src={CheckIcon} className={isSecondSelected ? 'headerSelectedCheck' : ''} />
+          </div>
+          <div className='cardContactType'>
+            <text onClick={() => setTypeSelected(3)} className={typeSelected === 3 ? 'headerSelected' : ''}>Design</text>
+            <div className={typeSelected === 3 ? 'headerSelectedTriangle' : ''} />
+            <img src={CheckIcon} className={isThirdSelected ? 'headerSelectedCheck' : ''} />
+          </div>
+        </div>
+        <div className='cardContactContent'>
+          <div>
+            <text className={typeSelected === 1 ? 'textSelected' : ''}>Está precisando de um site moderno, interativo e que te ajude a escalar ou consolidar seus negócios? Nós podemos te ajudar!</text>
+            <text className={typeSelected === 2 ? 'textSelected' : ''}>Está procurando uma maneira de transformar sua ideia em um aplicativo mobile? Você está no lugar certo!</text>
+            <text className={typeSelected === 3 ? 'textSelected' : ''}>Gostaria de criar uma identidade visual para impactar diretamente no sucesso de seus negócios? você veio ao lugar certo!</text>
+          </div>
+          <div className='cardContactButton'>
+            <button
+              onClick={() => { selectItem() }}
+              className={
+                typeSelected === 1 && isFirstSelected ? 'typeChecked'
+                  : typeSelected === 2 && isSecondSelected ? 'typeChecked'
+                    : typeSelected === 3 && isThirdSelected ? 'typeChecked'
+                      : ''
+              }
+            >
+              {
+                typeSelected === 1 && isFirstSelected ? 'Desfazer'
+                  : typeSelected === 2 && isSecondSelected ? 'Desfazer'
+                    : typeSelected === 3 && isThirdSelected ? 'Desfazer'
+                      : 'Tenho interesse!'
+              }
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='bottomRightColumn'>
+        <text className={quantitySelected === 0 ? 'hideText' : ''}>{quantitySelected} de 3 selecionado(s)</text>
+        <div>
+          <button onClick={() => setIsFormAvailable(true)}>Solicitar contato<img src={RightArrow} /></button>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,7 +3,8 @@ import OdicHeader from '../../components/Header/OdicHeader'
 import { Row, Col } from 'antd'
 import { Link } from 'react-router-dom'
 
-import CheckIcon from '../../assets/checkIcon.svg'
+import ContactRightColumn from '../../components/ContactRightCol'
+import ContactForm from '../../components/ContactForm'
 import Smile from '../../assets/smileIcon.svg'
 
 import './contactStyle.css'
@@ -15,14 +16,22 @@ export default function Contact () {
   const [isSecondSelected, setIsSecondSelected] = useState(false)
   const [isThirdSelected, setIsThirdSelected] = useState(false)
 
-  const [quatitySelected, setQuatitySelected] = useState(0)
+  const [quantitySelected, setQuantitySelected] = useState(0)
+
+  const [isFormAvailable, setIsFormAvailable] = useState(false)
 
   function selectItem () {
     if (typeSelected === 1) {
+      if (!isFirstSelected) setQuantitySelected(quantitySelected + 1)
+      else setQuantitySelected(quantitySelected - 1)
       setIsFirstSelected(!isFirstSelected)
     } else if (typeSelected === 2) {
+      if (!isSecondSelected) setQuantitySelected(quantitySelected + 1)
+      else setQuantitySelected(quantitySelected - 1)
       setIsSecondSelected(!isSecondSelected)
     } else if (typeSelected === 3) {
+      if (!isThirdSelected) setQuantitySelected(quantitySelected + 1)
+      else setQuantitySelected(quantitySelected - 1)
       setIsThirdSelected(!isThirdSelected)
     }
   }
@@ -46,61 +55,11 @@ export default function Contact () {
           </div>
         </Col>
         <Col span={10} className='rightCol'>
-          <div className='divText'>
-            <text>Gostaria de criar algo conosco?</text>
-            <text><text>É muito simples</text>, basta selecionar as</text>
-            <text>opções abaixo:</text>
+          <div className={isFormAvailable ? 'hide' : ''}>
+            <ContactRightColumn setTypeSelected={setTypeSelected} typeSelected={typeSelected} isFirstSelected={isFirstSelected} isSecondSelected={isSecondSelected} isThirdSelected={isThirdSelected} selectItem={selectItem} quantitySelected={quantitySelected} setIsFormAvailable={setIsFormAvailable} />
           </div>
-          <div className='cardContact'>
-            <div className='cardContactHeader'>
-              <div className='cardContactType'>
-                <text onClick={() => setTypeSelected(1)} className={typeSelected === 1 ? 'headerSelected' : ''}>Web</text>
-                <div className={typeSelected === 1 ? 'headerSelectedTriangle' : ''} />
-                <img src={CheckIcon} className={isFirstSelected ? 'headerSelectedCheck' : ''} />
-              </div>
-              <div className='cardContactType'>
-                <text onClick={() => setTypeSelected(2)} className={typeSelected === 2 ? 'headerSelected' : ''}>Mobile</text>
-                <div className={typeSelected === 2 ? 'headerSelectedTriangle' : ''} />
-                <img src={CheckIcon} className={isSecondSelected ? 'headerSelectedCheck' : ''} />
-              </div>
-              <div className='cardContactType'>
-                <text onClick={() => setTypeSelected(3)} className={typeSelected === 3 ? 'headerSelected' : ''}>Design</text>
-                <div className={typeSelected === 3 ? 'headerSelectedTriangle' : ''} />
-                <img src={CheckIcon} className={isThirdSelected ? 'headerSelectedCheck' : ''} />
-              </div>
-            </div>
-            <div className='cardContactContent'>
-              <div>
-                <text className={typeSelected === 1 ? 'textSelected' : ''}>Está precisando de um site moderno, interativo e que te ajude a escalar ou consolidar seus negócios? Nós podemos te ajudar!</text>
-                <text className={typeSelected === 2 ? 'textSelected' : ''}>Está procurando uma maneira de transformar sua ideia em um aplicativo mobile? Você está no lugar certo!</text>
-                <text className={typeSelected === 3 ? 'textSelected' : ''}>Gostaria de criar uma identidade visual para impactar diretamente no sucesso de seus negócios? você veio ao lugar certo!</text>
-              </div>
-              <div className='cardContactButton'>
-                <button
-                  onClick={() => { selectItem() }}
-                  className={
-                    typeSelected === 1 && isFirstSelected ? 'typeChecked'
-                      : typeSelected === 2 && isSecondSelected ? 'typeChecked'
-                        : typeSelected === 3 && isThirdSelected ? 'typeChecked'
-                          : ''
-                  }
-                >
-                  {
-                    typeSelected === 1 && isFirstSelected ? 'Desfazer'
-                      : typeSelected === 2 && isSecondSelected ? 'Desfazer'
-                        : typeSelected === 3 && isThirdSelected ? 'Desfazer'
-                          : 'Tenho interesse!'
-                  }
-                </button>
-              </div>
-            </div>
-            <div>
-              <text>
-                {
-
-                }
-              </text>
-            </div>
+          <div className={!isFormAvailable ? 'hide' : ''}>
+            <ContactForm />
           </div>
         </Col>
       </Row>
